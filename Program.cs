7fa037b42f0120aedbe95066d566a7eb28136512
Nicolas Leal <nicoleal14h@gmail.com>
2022-06-30 13:16:21 -0300

    ultimo commit

--- Program.cs
+++ Program.cs
@@ -4,59 +4,83 @@ using System;
 using System.IO; // uso la libreria para el manejo de archivos
 using System.Text.Json;
 using System.Text.Json.Serialization;
-internal class Program
+//inicio de la creacion de los productos
+Random aux= new Random();
+int ano, dia, mes;
+List<Productos> elementos= new List<Productos>();
+string archivo="productos.json";
+if (!File.Exists(archivo))
 {
-    private static void Main(string[] args)
-    {
-        // string [] Discos= Directory.GetLogicalDrives();
-        // foreach (string disco in Discos)
-        // {
-        //     System.Console.WriteLine(disco);
-        // }
-        System.Console.WriteLine("Dame el nombre de la carpeta de la cual queres tener la info, ubicada en el disco D");
-        string nombreCarpeta=Console.ReadLine();
-        //string ruta="D:\\"+nombreCarpeta;
-        if (Directory.Exists("D:\\"+nombreCarpeta))
-        {
-            
-            System.Console.WriteLine("Directorios pertenecientes a la carpeta:");
-            List<string> subcarpetas = Directory.GetDirectories("D:\\"+nombreCarpeta).ToList();
-            foreach (string buenas in subcarpetas)
-            {
-                System.Console.WriteLine(buenas);
-            }
-            System.Console.WriteLine("Los archivos pertenecientes a la carpeta son: ");
-            List<string> archivos = Directory.GetFiles("D:\\"+nombreCarpeta).ToList();
-            string nuevoArchivo="index.json";
-            if (!File.Exists(nuevoArchivo))
-            {
-                File.Create(nuevoArchivo);
-            }
-            //string winDir=Environment.GetEnvironmentVariable("windir");
-            string aux;
-            //escribo en el archivonuevoArchivocada archivo perteneciente a la carpeta
-            string [] auxDos= new string[archivos.Count];
-            int i=0;
-            foreach (string item in archivos)
-            {
-                System.Console.WriteLine(item);
-                FileInfo fileop= new FileInfo(item);
-                System.Console.WriteLine();
-                //System.Console.WriteLine(fileop.FullName+","+fileop.Length+","+fileop.LastAccessTime.ToString("ddMMyyyy")+","+fileop.Name+","+fileop.Extension);
-                auxDos[i]=fileop.FullName+","+fileop.Length+","+fileop.LastAccessTime.ToString("ddMMyyyy")+","+fileop.Name+","+fileop.Extension+"\n";
-                
-                i++;
-            }
-            //File.WriteAllLines(nuevoArchivo,auxDos);
-            string ArchivoJson= JsonSerializer.Serialize(auxDos);
-            File.WriteAllText(nuevoArchivo,ArchivoJson);
-            //obtengo las lineas escritas en el archivo index.cvs
-            System.Console.WriteLine("Lineas de el nuevo archivo: ");
-            System.Console.WriteLine(ArchivoJson);
+    File.Create(archivo);
+}
+for (int i = 0; i < 2; i++)
+{
+    //elementos[i]=new Productos();
+    System.Console.WriteLine("Nombre de el producto");
+    string Nombres=Console.ReadLine();
+    System.Console.WriteLine("Año de creacion: ");
+    ano=Int32.Parse(Console.ReadLine());
+    System.Console.WriteLine("mes de creacion: ");
+    mes=Int32.Parse(Console.ReadLine());
+    System.Console.WriteLine("dia de creacion: ");
+    dia=Int32.Parse(Console.ReadLine());
+    DateTime auxDos= new DateTime(ano, mes,dia);
+    // elementos[i].FechaVencimiento=auxDos;
+    System.Console.WriteLine("Tamaño del producto");
+    string Tamanios=Console.ReadLine();
+    // elementos[i].aleatorios();
+    elementos.Insert(i,new Productos{Nombre=Nombres,FechaVencimiento=auxDos, Tamanio=Tamanios});
+    elementos[i].aleatorios();
+    string texto= JsonSerializer.Serialize(elementos[i]);
+}
+string textos= JsonSerializer.Serialize(elementos);
+//añado los datos a el archivo .json
+File.WriteAllText(archivo,textos);
+var listao= JsonSerializer.Deserialize<List<Productos>>(textos);
+for (int e = 0; e < 2; e++)
+{
+    System.Console.WriteLine("Nombre: "+ listao[e].Nombre);
+    System.Console.WriteLine("Fecha de nacimiento: "+ (listao[e].FechaVencimiento).ToString("MM/dd/yyyy"));
+    System.Console.WriteLine("Tamañio: "+ listao[e].Tamanio);
+    System.Console.WriteLine("Precio: "+ listao[e].Precio);
+}
+class Productos
+{
+    private string nombre;
+    public string Nombre{
+        get{
+            return nombre;
         }
-        else
-        {
-            System.Console.WriteLine("No existe dicho directorio");
+        set{
+            nombre=value;
         }
+    }
+    private DateTime fechaVencimiento;
+    public DateTime FechaVencimiento{
+        get{
+            return fechaVencimiento;
+        }
+        set{
+            fechaVencimiento=value;
+        }
+    }
+    private int precio;
+    public int Precio{
+        get{
+            return precio;
+        }
+    }
+    private string tamanio;
+    public string Tamanio{
+        get{
+            return tamanio;
+        }
+        set{
+            tamanio=value;
+        }
+    }
+    private Random aux=new Random();
+    public void aleatorios(){
+        this.precio=aux.Next(1000,5001);
     }
 }
